69eaeb3d110c02cc28ef985301103b6f272be7de
Lucia <llopeza@alumno.unsm.edu.pe>
2022-05-02 16:09:36 -0500

    instalación react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+        
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
