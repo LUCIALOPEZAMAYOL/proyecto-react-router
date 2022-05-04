320a27f4bae2155f8469bec848d2ca7d5b9ecb28
Lucia <llopeza@alumno.unsm.edu.pe>
2022-05-04 11:52:46 -0500

    Proyecto terminado

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,9 @@ const Navbar = () => {
         <nav className="ui raised very padded segment">
             <Link className="ui teal inverted segment" to="/">Lucia</Link>
             <div className="ui right floated header">
-                <button className="ui button"><Link to="/">Home</Link></button>
+                <button className="ui button"><NavLink to="/">Home</NavLink></button>
                 <button className="ui button"><NavLink to="/about">About</NavLink></button>
-                <button className="ui button"><Link to="/contact">Contact</Link></button>
+                <button className="ui button"><NavLink to="/contact">Contact</NavLink></button>
             </div>
 
         </nav>
